0f75eb488c9c3799abf63212e3aff4f4eb928c31
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-31 13:23:54 -0500

    Added show and index to Product Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TireMart::Application.routes.draw do
+  get "products/index"
+  get "products/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
